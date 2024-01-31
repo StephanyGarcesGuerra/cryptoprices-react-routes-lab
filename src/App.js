c39182e5d3be09e6784b6608cb882c46d26c5e21
StephanyGarcesGuerra <stephanygarces96@gmail.com>
2024-01-31 15:56:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+
+import {Route, Routes} from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Crypto Prices App </h1>
+      <Nav />
+      <Routes>
+        <Route path = '/' element={ <Main />} />
+        <Route path = '/currencies' element={ <Currencies />} />
+        <Route path = '/price' element={ <Price />} />
+        <Route path="/price/:symbol" element={<Price/>}/>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Nav (props) {
+    return( 
+        <div className="nav">
+
+            <Link to = '/'>
+                <div>
+                    CRYPTO PRICES
+                </div>
+            </Link>
+
+            <Link to ='/currencies'>
+                <div> 
+                    Currencies
+                </div>
+            </Link>
+
+        </div>
+    );
+}
+
+export default Nav;
